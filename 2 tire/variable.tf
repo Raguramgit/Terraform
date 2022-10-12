df145f2f6bec1cf1dbb61f4bf1e18f39a0321a83
Raguraman01 <raguram7393@gmail.com>
2022-10-12 08:04:33 +0200

    VPC committed

--- 2 tire/variable.tf
+++ 2 tire/variable.tf
@@ -1,21 +1,28 @@
 variable  "myvpc" {
     default = "myvpc"
+    type = string
 }
 variable "environment"{
     default = "prod"
+    type = string
 }
 variable "subnet" {
     default = "subnet"
+    type = string
 }
 variable "gateway" {
     default = "gateway"
+    type = string
 }
 variable "route_table" {
     default = "route_table"
+    type = string
 }
 variable "mysecgroup" {
     default = "mysecgroup"
+    type = string
 }
 variable "name" {
     default = "Terraform"
+    type = string
 }
